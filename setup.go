17714fd2ae4950fd8758730d830909f8a357d595
wessie <github@wessie.info>
2014-05-24 18:48:24 +0200

    Setup now follows the configuration when it comes to the http server.
    Setup will now listen on a separate address if given for the HTTP server.
    
    Note: This should be a separate function but will be here for the time being.

--- setup.go
+++ setup.go
@@ -3,6 +3,7 @@ package sirencast
 import (
 	"errors"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/Wessie/sirencast/config"
@@ -13,11 +14,31 @@ func Setup() (*config.Config, error) {
 		return nil, errors.New("unable to load configuration")
 	}
 
+	// TODO: Move all of this into reusable functions
 	// Setup a listener for HTTP requests
-	httpListener := NewHTTPListener()
-	DefaultDetectors.Default = httpListener.Handler
+	if config.Active.HTTPServer.Disabled {
+		return config.Active, nil
+	}
+
+	var l net.Listener
+	// Setup a protocol detector default and a fake listener
+	// for HTTP if the configuration tells us to not run the
+	// HTTP server on a different address.
+	if config.Active.HTTPServer.Addr == "" {
+		httpListener := NewHTTPListener(config.Active.Addr)
+		DefaultDetectors.Default = httpListener.Handler
+		l = httpListener
+	} else {
+		var err error
+		l, err = net.Listen("tcp", config.Active.HTTPServer.Addr)
+		if err != nil {
+			log.Printf("HTTP unable to listen on '%s': %s\n", config.Active.HTTPServer.Addr, err)
+		}
+	}
+	log.Printf("HTTP server listening on '%s'\n", l.Addr())
+
 	go func() {
-		if err := http.Serve(httpListener, nil); err != nil {
+		if err := http.Serve(l, nil); err != nil {
 			log.Println("HTTP server exited:", err)
 		}
 	}()
