4dad3271a5b2791e1f049b821c4659b54a7755fc
wessie <github@wessie.info>
2014-05-25 23:24:01 +0200

    Fixed a bug in (*SirenConn).Read
    The bug was caused by returning the original `n` and `err` from the
    peeker.Read, this `err` would've been non-nil and caused the reader
    to receive an error that wasn't ment to be returned.

--- conn.go
+++ conn.go
@@ -21,6 +21,8 @@ type Conn struct {
 	handler ConnHandler
 }
 
+// serve calls the appointed handler with sc as argument, it recovers from any panics
+// that occur inside the handler to avoid the whole server going down.
 func (sc *Conn) serve() {
 	defer func() {
 		if err := recover(); err != nil {
@@ -37,19 +39,24 @@ func (sc *Conn) Read(b []byte) (n int, err error) {
 	n, err = sc.reader.Read(b)
 
 	if err != nil {
-		if sc.reader == sc.peeker {
-			sc.reader = sc.conn
-
-			// If we read some bytes before, return those
-			if n > 0 {
-				return
-			}
-
-			// otherwise proxy ourself to the new reader
-			return sc.reader.Read(b)
+		// We're already reading from the net.Conn, so
+		// we can return whatever the net.Conn returned.
+		if sc.reader != sc.peeker {
+			return
 		}
 
-		return
+		// Otherwise we're going to swap to the net.Conn now
+		// and continue reading.
+		sc.reader = sc.conn
+
+		// If we read some bytes before, return those first
+		// before continueing.
+		if n > 0 {
+			return n, nil
+		}
+
+		// otherwise proxy ourself to the new reader
+		return sc.reader.Read(b)
 	}
 
 	return
